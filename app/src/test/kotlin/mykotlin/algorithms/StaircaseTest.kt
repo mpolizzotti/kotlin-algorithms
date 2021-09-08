29c806a60f0c03c2667fc66618e121c3061f073b
Matthew Polizzotti <MPolizzotti-consultant@Scholastic.com>
2021-09-07 20:56:45 -0700

    feat(staircase): add solution

---
+++ app/src/test/kotlin/mykotlin/algorithms/StaircaseTest.kt
@@ -0,0 +1,36 @@
+package mykotlin.algorithms
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StaircaseTest {
+    @Test
+    fun `should return an object representing a right-justified staircase 4 levels deep`(): Unit {
+        val n = 4;
+        val staircase: Staircase = Staircase()
+        val result: HashMap<String, String> = staircase.execute(n)
+        val expected: HashMap<String, String> = HashMap()
+        expected["1"] = "   #"
+        expected["2"] = "  ##"
+        expected["3"] = " ###"
+        expected["4"] = "####"
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `should return an object representing a right-justified staircase 6 levels deep`(): Unit {
+        val n = 6;
+        val staircase: Staircase = Staircase()
+        val result: HashMap<String, String> = staircase.execute(n)
+        val expected: HashMap<String, String> = HashMap()
+        expected["1"] = "     #"
+        expected["2"] = "    ##"
+        expected["3"] = "   ###"
+        expected["4"] = "  ####"
+        expected["5"] = " #####"
+        expected["6"] = "######"
+
+        assertEquals(expected, result)
+    }
+}
