657d0c30fc4a51db6202b84e5a3d8f9d4df08503
Matthew Polizzotti <MPolizzotti-consultant@Scholastic.com>
2022-04-20 08:32:13 -0700

    feat(binarySearch): add solution

---
+++ app/src/main/kotlin/mykotlin/algorithms/BinarySearch.kt
@@ -0,0 +1,25 @@
+package mykotlin.algorithms
+
+import kotlin.math.floor
+
+internal fun binarySearch(arr: List<Int>, num: Int): Int {
+    var startIndex = 0
+    var endIndex = arr.size - 1
+
+    while (startIndex <= endIndex ) {
+        val middleIndex = floor((startIndex + ((endIndex - startIndex) / 2)).toDouble()).toInt();
+
+        if (num == arr[middleIndex]) {
+            return middleIndex
+        }
+
+        if (num > arr[middleIndex]) {
+            startIndex = middleIndex + 1
+        } else {
+            endIndex = middleIndex - 1
+        }
+    }
+
+    return -1
+}
+
